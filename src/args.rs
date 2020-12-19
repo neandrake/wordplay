3f13d2f86e273377a66baf8171612b0e854ae2c8
Christopher Speck <die.drachen@gmail.com>
2020-12-19 17:24:22 -0500

    Correct formatting of help text

--- src/args.rs
+++ src/args.rs
@@ -8,14 +8,19 @@ pub struct Arguments {
 
 #[derive(StructOpt, Debug)]
 pub enum Command {
+    #[structopt(verbatim_doc_comment)]
     /// Solves the New York Times' Spelling Bee puzzle where they present 7 letters
     /// and playing involves inputting as many valid words that can be built from
-    /// those letters, with some restrictions:
+    /// those letters.
+    /// 
+    /// See: https://www.nytimes.com/puzzles/spelling-bee
+    ///
+    /// Words accepted as answers have these restrictions:
     ///  - Words must be at least 4 letters or more
     ///  - Words may contain the same letter multiple times
     ///  - Words must use the "golden" letter at least once
     ///  - There will be at least one answer which uses all 7 letters
-    ///  See: https://www.nytimes.com/puzzles/spelling-bee
+    ///  - Proper-nouns are not accepted
     Beehive {
         /// The single letter required to be in each word.
         queen: char,
